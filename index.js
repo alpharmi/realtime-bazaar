1ff5a553d2eca966b236335ff8d6b34cb04c8da2
alpha <Gianni.s177@outlook.com>
2022-01-24 01:04:11 +1100

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ function requestBazaar() {
                             productElement.children["profitMargin"].textContent = `Profit Margin: $${String(productMargin).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`
                             productElement.children["profitMargin1024x"].textContent = `Profit Margin (1024x): $${String(productMargin * 1024).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`
     
-                            productScore = -(productMargin - (quanitySelling - quanitySelling))
+                            productScore = -(productMargin - (quanitySelling - quanityBuying))
     
                             productElement.style.order = productScore
 
